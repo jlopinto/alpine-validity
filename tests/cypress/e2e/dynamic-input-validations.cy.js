8e5aa3906e81bca0573af2e74531415ca2c96455
Julien Lopinto <julien.lopinto@gmail.com>
2023-03-17 15:25:37 +0100

    Adds tests for dynamically append and remove form elements

---
+++ tests/cypress/e2e/dynamic-input-validations.cy.js
@@ -0,0 +1,27 @@
+describe('Test validation on dynamic inputs', () => {
+  beforeEach(() => {
+    cy.visit('/spec.html')
+  })
+
+  it('displays native validation message on form submit', () => {
+    cy.get('#form-4').within(() => {
+      cy.get('.add-btn').click()
+      cy.get('[type="submit"]').click()
+
+      cy.get('.form-row').last().within(() => {
+        cy.get('.input-name .errorMessage').contains('Please fill out this field.')
+        cy.get('.input-mail .errorMessage').contains('Please fill out this field.')
+
+        cy.get('.input-name input').type('robert')
+        cy.get('.input-name .errorMessage').should('be.empty')
+
+        cy.get('.input-mail input').type('robert')
+        cy.get('.input-mail .errorMessage').contains("Please include an '@' in the email address. 'robert' is missing an '@'.")
+
+        cy.get('.input-mail input').type('@mail.com')
+        cy.get('.input-mail .errorMessage').should('be.empty')
+      })
+      cy.get('.form-status').contains('valid')
+    })
+  })
+})
