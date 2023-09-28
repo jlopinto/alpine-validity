fdcb09e69374aa29763982032844284ff2b585a0
Julien Lopinto <julien.lopinto@gmail.com>
2023-09-28 10:39:52 +0200

    WIP

--- src/validity.js
+++ src/validity.js
@@ -1,184 +1,185 @@
 /* global CustomEvent, MutationObserver */
 
+/* todo: use Map (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) */
+const config = {}
+
 const checkElementValidity = (target) => {
-  if (!target.name) return
-
+  const constraints = {
+    ...config[target.type],
+    ...target.form.__controls,
+    ...target.__controls
+  }
   const validityStates = target.validity
-
-  /* retrieve messages and controls from the element or the parent form */
-  const messages = target.__messages || target.form.__messages || []
-  const controls = target.__controls || target.form.__controls || []
 
   let errors = []
 
   /* reset the input validity['customError'] state to false */
-  target.setCustomValidity('')
-  console.log(target.name)
+  target.setCustomValidity("")
   /* First, checks for native errors */
-  for (const state in validityStates) {
-    // console.log(state, validityStates[state])
-    if (validityStates[state] === true) {
-      console.log(messages[state] || target.validationMessage)
-      errors.push({ target, name: state, valid: messages[state], message: messages[state] || target.validationMessage })
+  if (!validityStates["valid"]) {
+    for (const state in validityStates) {
+      if (validityStates[state] === true) {
+        errors.push({
+          target,
+          name: state,
+          valid: false,
+          message:
+            constraints && constraints[state]
+              ? constraints[state]?.message
+              : target.validationMessage
+        })
+      }
     }
   }
 
-  console.log(errors)
-
   /* then check for custom errors */
-  errors = Object.assign(errors, controls.map((control) => {
-    return { target, name: control.name, valid: control(target.value), message: messages[control.name] }
-    // if (!control(target.value)) {
-    //   /* set the input validity['customError'] state to true */
-    //   errorMessage = messages[control.name]
-    //   target.setCustomValidity(errorMessage)
-    // }
-  }))
-  console.log(errors)
-  /* dispatch the validation event with the error as detail */
-  target.dispatchEvent(new CustomEvent('validation', {
-    detail: { errors, validity: target.form.checkValidity() },
-    bubbles: true
-  }))
-}
+  errors = [
+    ...errors,
+    ...Object.keys(constraints)
+      .filter((constraintName) => {
+        const { validationTest, message } = constraints[constraintName]
+        return typeof validationTest === "function"
+      })
+      .map((constraintName) => {
+        const { validationTest, message } = constraints[constraintName]
+        return {
+          target,
+          name: constraintName,
+          valid: validationTest(target.value),
+          message
+        }
+      })
+  ]
 
-/* handles x-validity:messages="{control1: "control message 1", control2: "control message 2"}" */
-const handleMessages = (el, expression, evaluateLater, effect) => {
-  const getMessages = evaluateLater(expression)
-  effect(() => {
-    getMessages(data => {
-      el.__messages = data
+  /* dispatch the validation event with the errors as detail */
+  target.dispatchEvent(
+    new CustomEvent("validation", {
+      detail: { errors, validity: target.form.checkValidity() },
+      bubbles: true
     })
-  })
+  )
 }
 
-/* handles x-validity:controls="[control1, control2, ...]" */
+/* handles x-validity:controls="{...}" */
 const handleControls = (el, expression, evaluateLater, effect) => {
   const getControls = evaluateLater(expression)
   effect(() => {
-    getControls(data => {
+    getControls((data) => {
       el.__controls = data
     })
   })
 }
 
 /* initiate form element */
-const handleElement = (el, Alpine) => Alpine.bind(el, {
-  'x-data' () {
-    return {
-      __modified: false,
-      __blurred: false,
-      get __canCheck () {
-        return this.__modified && this.__blurred
-      }
-    }
-  },
-  'x-init' () {
-    this.$el.__messages = null
-    this.$el.__controls = null
-  },
-  '@input.once' ({ target }) {
-    Alpine.$data(target).__modified = true
-  },
-  '@focusout.once' ({ target }) {
-    Alpine.$data(target).__blurred = true
-  }
-})
-
-/* handles x-validity */
-const handleRoot = (form, Alpine) => {
-  const mutations = new MutationObserver(([mutation]) => {
-    const appendedControls = Array.from(form.elements).filter((element) =>
-      Array.from(mutation.addedNodes)
-        .filter((addedNode) => {
-          return addedNode.contains(element).length && element.tagName !== 'BUTTON' && (element.hasAttribute('x-validity:messages') || element.hasAttribute('x-validity:controls'))
-        })
-    )
-
-    appendedControls.forEach((appendedControl) => handleElement(appendedControl, Alpine))
-
-    form.dispatchEvent(new CustomEvent('validation', {
-      detail: { validity: form.checkValidity(), appendedControls }
-    }))
-  })
-
-  Alpine.bind(form, {
-    'x-data' () {
+const handleElement = (el, Alpine) =>
+  Alpine.bind(el, {
+    "x-data"() {
       return {
-        __errors: new Map(),
-        messages: [],
-        get __formElements () {
-          return Array.from(this.$el.elements)
-            .filter(element => element.willValidate)
+        isModified: false,
+        isBlurred: false,
+        get() {
+          return this.isModified && this.isBlurred
         }
       }
     },
-    'x-init' () {
-      /* watch for childs mutations */
-      mutations.observe(this.$el, { childList: true })
-      this.__formElements.forEach(el => handleElement(el, Alpine))
+    "x-init"() {
+      this.$el.__controls = null
     },
-    ':novalidate': true,
-    '@validation' (ev) {
+    "@input.once"({ target }) {
+      Alpine.$data(target).isModified = true
+    },
+    "@focusout.once"({ target }) {
+      Alpine.$data(target).isBlurred = true
+    }
+  })
+
+/* handles x-validity */
+const handleRoot = (form, expression, evaluateLater, effect, Alpine) => {
+  const formFieldsMutations = new MutationObserver(([mutation]) => {
+    const appendedControls = Array.from(form.elements).filter((element) => {
+      return Array.from(mutation.addedNodes).filter((addedNode) => {
+        return (
+          addedNode.contains(element).length &&
+          !(element.tagName instanceof HTMLButtonElement) &&
+          (element.hasAttribute("x-validity:messages") ||
+            element.hasAttribute("x-validity:controls"))
+        )
+      })
+    })
+
+    appendedControls.forEach((appendedControl) =>
+      handleElement(appendedControl, Alpine)
+    )
+
+    form.dispatchEvent(
+      new CustomEvent("validation", {
+        detail: { validity: form.checkValidity(), appendedControls }
+      })
+    )
+  })
+
+  handleControls(form, expression, evaluateLater, effect)
+
+  Alpine.bind(form, {
+    "x-data"() {
+      return {
+        formErrors: new Map(),
+        messages: [],
+        get getFormElement() {
+          return Array.from(this.$el.elements).filter(
+            (element) =>
+              element.willValidate && !(element instanceof HTMLButtonElement)
+          )
+        }
+      }
+    },
+    "x-init"() {
+      /* watch for childs mutations */
+      formFieldsMutations.observe(this.$el, { childList: true })
+      this.getFormElement.forEach((el) => handleElement(el, Alpine))
+    },
+    ":novalidate": true,
+    "@validation"(ev) {
       const target = ev.target
       if (target.name) {
-        this.__errors.set(target.name, ev.detail.errors)
+        this.formErrors.set(target.name, ev.detail.errors)
       }
     },
-    '@submit' () {
+    "@submit"() {
       /* trigger a full validation check */
-      this.__formElements
-        .forEach(element => {
-          Alpine.$data(element).__blurred = true
-          checkElementValidity(element)
-        })
+      this.getFormElement.forEach((element) => {
+        Alpine.$data(element).isBlurred = true
+        checkElementValidity(element)
+      })
     },
-    '@input' ({ target }) {
-      if (Alpine.$data(target).__canCheck) {
-        checkElementValidity(target)
-      }
+    "@input"({ target }) {
+      checkElementValidity(target)
     },
-    '@reset' () {
-      this.__errors.clear()
-      this.__formElements
-        .forEach(element => {
-          Alpine.$data(element).__modified = false
-          Alpine.$data(element).__blurred = false
-          element.setCustomValidity('')
-        })
+    "@reset"() {
+      this.formErrors.clear()
+      this.getFormElement.forEach((element) => {
+        Alpine.$data(element).isModified = false
+        Alpine.$data(element).isBlurred = false
+        element.setCustomValidity("")
+      })
     }
   })
 }
 
-export default (Alpine) => {
-  Alpine.directive('validity', (el, { value, expression }, { evaluateLater, effect }) => {
-    switch (value) {
-      case 'messages':
-        handleMessages(el, expression, evaluateLater, effect)
-        break
-      case 'controls':
-        handleControls(el, expression, evaluateLater, effect)
-        break
-      default:
-        handleRoot(el, Alpine)
-        break
-    }
-  })
+export default (Alpine, customConfig = {}) => {
+  Object.assign(config, customConfig)
 
-  Alpine.magic('validity', (control) => subject => {
-    // Alpine.$data(fieldName).__errors.get(subject)
-    return {
-      get errors () {
-        return Alpine.$data(control).__errors.get(subject)
-      },
-      get firstError () {
-        return Alpine.$data(control).__errors.get(subject)
-      },
-      error (name) {
-        const errors = Alpine.$data(control).__errors.get(subject)
-        console.log(errors)
-        return errors ? errors.find(error => error.name === name) : []
-      }
+  Alpine.directive(
+    "validity",
+    (el, { value, expression }, { evaluateLater, effect }) => {
+      if (value === "controls")
+        handleControls(el, expression, evaluateLater, effect)
+      if (value === null)
+        handleRoot(el, expression, evaluateLater, effect, Alpine)
     }
+  )
+
+  Alpine.magic("validity", (control) => (subject) => {
+    return Alpine.$data(control).formErrors.get(subject)
   })
 }
